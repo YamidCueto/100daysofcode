a2113c2957290f48227d7b2c040dc573ba4c6339
YamiDev <ycueto0@misena.edu.co>
2024-05-19 14:41:21 -0500

    initial commit

---
+++ src/day1/DayOne.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class DayOne {
+    /**
+     * Returns a string "Hello Day One!".
+     *
+     * @return "Hello Day One!"
+     */
+    public static String getHelloDayOne() {
+        return "Hello Day One!";
+    }
+}
---
+++ src/main/Main.java
@@ -0,0 +1,9 @@
+package main;
+
+import day1.DayOne;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(DayOne.getHelloDayOne());
+    }
+}
